7ae90dd81b36fddda62d0b615771e312d10fb7c4
JEVARAAJKUMAR SELVARAJAH <jeva_sanjay@hotmail.com>
2018-11-01 12:20:33 +0000

    Edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_users, only: [:edit,:update]
+  
   def new
     @user = User.new
   end
@@ -15,12 +17,29 @@ class UsersController < ApplicationController
   end
  end
  
+ def edit
+ end
+ 
+ def update
+ 
+ if @user.update(user_params)
+   flash[:success] = "Your account was updated sucessfully"
+   redirect_to articles_path
+ else
+   render 'edit'
+ end
+end 
+ 
  private
  
  def user_params
   params.require(:user).permit(:username, :email, :password)
  end
 
+ def set_users
+  @user = User.find(params[:id])
+ end
+ 
 end
 
  
